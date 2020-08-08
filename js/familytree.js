c330f65c7c724898cd74e185cea15f2df684d09d
Berkin Cetin <bcetin14@gmail.com>
2020-08-08 13:23:12 -0400

    family tree

--- js/familytree.js
+++ js/familytree.js
@@ -11,7 +11,7 @@ var chart = new OrgChart(document.getElementById("js-tree"), {
         { id: 6, pid: 2, name: "Ali" },
         { id: 7, pid: 2, name: "Basri" },
 
-        { id: 8, pid: 3, name: "Sitki" },
+        { id: 8, pid: 3, name: "Sitki K. Umgusum" },
         { id: 9, pid: 3, name: "Ismail" },
         { id: 10, pid: 3, name: "Sefika" },
         { id: 11, pid: 3, name: "Huseyin" },
@@ -34,6 +34,24 @@ var chart = new OrgChart(document.getElementById("js-tree"), {
         { id: 25, pid: 6, name: "Basri" },
         { id: 26, pid: 6, name: "Gonul" },
         { id: 27, pid: 6, name: "Ismail" },
+
+        { id: 28, pid: 8, name: "Remzi K. Emine" },
+        { id: 29, pid: 28, name: "Dilber d.Erdinc" },
+        { id: 30, pid: 28, name: "Sitki K. Turkan" },
+        { id: 31, pid: 29, name: "Zerrin" },
+        { id: 32, pid: 29, name: "Berkin" },
+        { id: 33, pid: 30, name: "Tolga" },
+        { id: 34, pid: 30, name: "Saleh" },
+
+        { id: 35, pid: 8, name: "Safinaz d. Mustafa" },
+        { id: 36, pid: 35, name: "Emine d. Aydin" },
+        { id: 37, pid: 35, name: "Ahmet K. Sevgul" },
+        { id: 38, pid: 36, name: "Emel" },
+        { id: 39, pid: 36, name: "Sadik" },
+        { id: 40, pid: 37, name: "Safinaz" },
+        { id: 41, pid: 37, name: "Gokhan" },
+
+
 
     ]
     
